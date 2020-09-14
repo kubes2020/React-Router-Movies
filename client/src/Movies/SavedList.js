68a91abe170bf58140913cef450d48d5b659f971
Brian Kubes <briankubes@gmail.com>
2020-09-14 14:37:59 -0700

    added key to SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -6,7 +6,9 @@ export default function SavedList(props) {
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map((movie) => (
-        <span className="saved-movie">{movie.title}</span>
+        <span key={movie.id} className="saved-movie">
+          {movie.title}
+        </span>
       ))}
       <Link to="/" className="home-button">
         Home
