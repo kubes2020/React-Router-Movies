9a26f9cb06510b25a24d91ce1a5d60a3f54d2cf4
Brian Kubes <briankubes@gmail.com>
2020-09-14 14:19:26 -0700

    movie component seems to be working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Route, useMatchRoute, useParams } from "react-router-dom";
+import { Route, useMatchRoute, useParams, Switch } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -42,13 +42,15 @@ export default function App() {
       />
 
       <div>Replace this Div with your Routes</div>
-      <Route path="/movies/:ID">
-        <Movie />
-      </Route>
+      <Switch>
+        <Route path="/movies/:ID">
+          <Movie />
+        </Route>
 
-      <Route path="/">
-        <MovieList movies={movieList} />
-      </Route>
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
