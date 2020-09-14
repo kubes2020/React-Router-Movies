f312cb2b59c1504b5644eba711009123b2e4adf1
Brian Kubes <briankubes@gmail.com>
2020-09-14 14:35:07 -0700

    Home link is working on SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export default function SavedList(props) {
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
+      {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <Link to="/" className="home-button">
+        Home
+      </Link>
+      {/* <div className="home-button">Home</div> */}
     </div>
   );
 }
